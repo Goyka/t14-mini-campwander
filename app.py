ccf12e50b91c09fb0aa24de4e6c6d299cdcdb483
김성호 <120389368+Banana-Master@users.noreply.github.com>
2023-08-09 21:45:46 +0900

    로그인 회원가입

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, session, redirect, url_for
+import bcrypt
 app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'
 
 from pymongo import MongoClient
 client = MongoClient('mongodb+srv://sparta:test@cluster0.qef1qmv.mongodb.net/?retryWrites=true&w=majority')
@@ -23,20 +25,55 @@ def join_post():
     pw_receive = request.form['pw_give']
     name_receive = request.form['name_give']
     phone_receive = request.form['phone_give']
+
+    # 비밀번호를 해싱하여 저장
+    hashed_pw = bcrypt.hashpw(pw_receive.encode('utf-8'), bcrypt.gensalt())
+
     doc = {
         'id':id_receive,  
-        'pw' : pw_receive,
+        'pw' : hashed_pw.decode('utf-8'),
         'name' : name_receive,
         'phone' : phone_receive   
-        }
+    }
     db.user.insert_one(doc)
     
     return jsonify({'msg': '회원가입 완료!'})
 
-@app.route("/bucket", methods=["GET"])
-def bucket_get():
-    all_buckets = list(db.bucket.find({},{'_id':False}))
-    return jsonify({'result': all_buckets})
+@app.route("/login/user", methods=["POST"])
+def login_post():
+    id_receive = request.json['id']
+    pw_receive = request.json['pw']
+    
+    user = db.user.find_one({'id': id_receive}, {'_id': False})
+    
+    if user and bcrypt.checkpw(pw_receive.encode('utf-8'), user['pw'].encode('utf-8')):
+        session['user_id'] = id_receive
+        session['user_name'] = user['name']  # 사용자 닉네임 세션에 저장
+        return jsonify({'success': True, 'message': '로그인 성공'})
+
+    else:
+        return jsonify({'success': False, 'message': '로그인 실패'})
+    
+@app.route("/logout")
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('home'))
+
+@app.route('/profile')
+def profile():
+    if 'user_name' in session:
+        # user_id = session['user_id']
+        user_name = session['user_name']
+        # 여기에서 해당 사용자 정보를 데이터베이스 등에서 가져와 사용할 수 있음
+        return f'안녕하세요, {user_name}님! 프로필 페이지입니다.'
+    else:
+        return '로그인이 필요합니다.'
+
+@app.route('/get_all_users')
+def all_users():
+    all_users = list(db.user.find({},{'_id':False}))
+    return jsonify({'result': all_users})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
--- static/js/join.js
+++ static/js/join.js
@@ -1,15 +1,74 @@
-
+let user_info_id = [];
 
 const join = () => {
     $('.main_join').hide();
     $('.join_info').css('display', 'flex');
+    // 몽고디비에 저장된 모든 회원정보 ID
+    fetch('/get_all_users').then(response => response.json()).then(data => {
+        // 가져온 회원 정보 출력
+        data['result'].forEach((x) => {
+            user_info_id.push(x['id'])
+        });
+    })
+    .catch(error => {
+        console.error('오류 발생:', error);
+    });
+}
+
+$(document).ready(function() {
+    $('#id').keyup(function() {
+        CheckId();
+    });
+
+    // 비밀번호 입력란에서 키보드 입력 시 이벤트 리스너 추가
+    $('#pw').keyup(function() {
+        checkPassword();
+    });
+
+    // 비밀번호 확인 입력란에서 키보드 입력 시 이벤트 리스너 추가
+    $('#pw2').keyup(function() {
+        checkPassword();
+    });
+});
+
+function CheckId() {
+    let id = $('#id').val();
+        for(let i = 0; i < user_info_id.length; i++) {
+            console.log(id)
+            $('#id').css('border', '1px solid #1559ff')
+            $('#id-match').css('display', 'none')
+            if(id === user_info_id[i] || id === '' || id.includes(' ')) {
+                $('#id').css('border', '1px solid red')
+                $('#id-match').css('display', 'inline')
+                $('#id-match').text('이 아이디는 사용하실 수 없습니다')
+                return;
+            }
+        }
+}
+
+function checkPassword() {
+    let pw = $('#pw').val();
+    let pw2 = $('#pw2').val();
+    $('#password-match').css('display', 'inline')
+    if (pw === pw2) {
+        $('#password-match').css('display', 'none');
+        $('#pw').css('border', '1px solid #1559ff');
+        $('#pw2').css('border', '1px solid #1559ff');
+    } else {
+        $('#password-match').text('비밀번호 불일치');
+        $('#password-match').css('color', 'red')
+    }
 }
 
 const user_join = () => {
-    let id= $('#id').val();
-    let pw= $('#pw').val();
-    let name= $('#name').val();
-    let phone= $('#phone').val();
+    let id = $('#id').val();
+    let pw = $('#pw').val();
+    let pw2 = $('#pw2').val();
+    if(pw !== pw2) {
+        return;
+    }
+    let name = $('#name').val();
+    let phone = $('#phone').val();
     let formData = new FormData();
     formData.append("id_give", id);
     formData.append("pw_give", pw);
@@ -19,4 +78,34 @@ const user_join = () => {
         alert(data["msg"]);
         window.location.reload();
     });
+}
+
+const login = () => {
+    let id = $('#id').val();
+    let pw = $('#pw').val();
+    let requestData = {
+        id: id,
+        pw: pw
+    };
+
+    fetch('/login/user', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestData)
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data['success']) {
+                console.log(data)
+                // alert(`${data['user_id']}님 환영합니다`);
+                window.location.href = '/';
+            } else {
+                alert('로그인 실패: ' + data['message']);
+            }
+        })
+        .catch(error => {
+            console.error('오류 발생:', error);
+        });
 }
