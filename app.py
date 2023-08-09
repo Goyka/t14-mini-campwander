531c56d252afe84f876bac6fd2dfca71b93bcfb5
김성호 <120389368+Banana-Master@users.noreply.github.com>
2023-08-09 17:33:32 +0900

    회원가입

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 from pymongo import MongoClient
 client = MongoClient('mongodb+srv://sparta:test@cluster0.qef1qmv.mongodb.net/?retryWrites=true&w=majority')
-db = client.dbsparta
+db = client.miniproject
 
 @app.route('/')
 def home():
@@ -17,6 +17,26 @@ def login():
 def join():
     return render_template('join.html')
 
+@app.route('/join/user', methods=["post"])
+def join_post():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+    name_receive = request.form['name_give']
+    phone_receive = request.form['phone_give']
+    doc = {
+        'id':id_receive,  
+        'pw' : pw_receive,
+        'name' : name_receive,
+        'phone' : phone_receive   
+        }
+    db.user.insert_one(doc)
+    
+    return jsonify({'msg': '회원가입 완료!'})
+
+@app.route("/bucket", methods=["GET"])
+def bucket_get():
+    all_buckets = list(db.bucket.find({},{'_id':False}))
+    return jsonify({'result': all_buckets})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
--- static/js/join.js
+++ static/js/join.js
@@ -4,3 +4,19 @@ const join = () => {
     $('.main_join').hide();
     $('.join_info').css('display', 'flex');
 }
+
+const user_join = () => {
+    let id= $('#id').val();
+    let pw= $('#pw').val();
+    let name= $('#name').val();
+    let phone= $('#phone').val();
+    let formData = new FormData();
+    formData.append("id_give", id);
+    formData.append("pw_give", pw);
+    formData.append("name_give", name);
+    formData.append("phone_give", phone);
+    fetch('/join/user', { method: "POST", body: formData, }).then((response) => response.json()).then((data) => {
+        alert(data["msg"]);
+        window.location.reload();
+    });
+}
