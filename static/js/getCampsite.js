0095ddf9ffbdd1e797de30b84f504d3843d8983e
김성호 <120389368+Banana-Master@users.noreply.github.com>
2023-08-10 18:14:56 +0900

    중간저장

--- static/js/getCampsite.js
+++ static/js/getCampsite.js
@@ -18,7 +18,7 @@ function getCampsite() {
       var imageSrc = "../static/img/map_marker.svg";
       (imageSize = new kakao.maps.Size(40, 50)), // 마커이미지의 크기입니다
         (imageOption = { offset: new kakao.maps.Point(27, 69) }); // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다
-
+      console.log(apiData[0])
       // 마커가 지도 위에 표시되도록 설정합니다
       for (let i = 0; i < apiData.length; i++) {
         if (apiData[i].lctCl == "산") {
@@ -26,6 +26,8 @@ function getCampsite() {
           let lon = apiData[i].mapX;
           let title = apiData[i].facltNm;
           let addr = apiData[i].addr1;
+          let tel = apiData[i].tel;
+          let homepage = apiData[i].homepage;
 
           var markerImage = new kakao.maps.MarkerImage(
             imageSrc,
@@ -33,10 +35,18 @@ function getCampsite() {
             imageOption
           );
           var content = `
-            <div class="info">
-                <div class="title">${title}</div>
-                <div>${addr}</div>
-            </div>`;
+          <div class="info">
+          <div class="title"><span>${title}</span></div>
+          <div class="address"><span>${addr}</span></div>
+          <div class="tel"><span>${tel !== '' ? tel : '전화정보가 없습니다'}</span></div>
+          <div class="homepage"><a href="${homepage}">홈페이지</a></div>
+      </div>`;
+    //         <div class="info">
+    //     <div class="title"><span>${title}</span></div>
+    //     <div class="address"><span>${addr}</span></div>
+    //     <div class="tel"><span>${tel}</span></div>
+    //     <div class="homepage"><a href="${homepage}">홈페이지</a></div>
+    // </div>
 
           var marker = new kakao.maps.Marker({
             //   map: map,
